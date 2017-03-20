3fd2da4e2ff4f9979264bace2c6f0c4c3bb5f3b2
David Hunt <davidpaulhunt@gmail.com>
2017-03-20 14:45:10 -0400

    cleaning up sender base tests

--- test/unit/senderBaseSpec.js
+++ test/unit/senderBaseSpec.js
@@ -1,13 +1,12 @@
-"use strict";
+const chai = require('chai');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const senderBasePath = '../../lib/SenderBase';
+const Constants = require('../../lib/Constants');
 
-var chai = require('chai'),
-    expect = chai.expect,
-    sinon = require('sinon'),
-    proxyquire = require('proxyquire'),
-    senderBasePath = '../../lib/SenderBase',
-    Constants = require('../../lib/Constants');
+const expect = chai.expect;
 
-describe('UNIT SenderBase', function () {
+describe('UNIT SenderBase', () => {
   // Use object to set arguments passed into callback
   var args = {};
   var requestStub = function (options, callback) {
@@ -16,35 +15,23 @@ describe('UNIT SenderBase', function () {
   };
   var SenderBase = proxyquire(senderBasePath, { 'request': requestStub });
 
-  describe('constructor', function () {
-    it.skip('should call new constructor if user does not', function() {
-      var senderBase = SenderBase();
-      expect(senderBase).to.not.be.undefined;
-      expect(senderBase).to.be.instanceOf(SenderBase);
-    });
-
-    it.skip('should create a SenderBase with key and options passed in', function() {
-      var options = {
+  describe('constructor', () => {
+    it('should create a SenderBase with key and options passed in', () => {
+      const options = {
         proxy: 'http://myproxy.com',
         maxSockets: 100,
-        timeout: 100
+        timeout: 100,
       };
 
-      var key = 'myApiKey',
-          senderBase = new SenderBase(key, options);
+      const key = 'myApiKey';
+      const senderBase = new SenderBase(key, options);
       expect(senderBase).to.be.instanceOf(SenderBase);
       expect(senderBase.key).to.equal(key);
       expect(senderBase.options).to.deep.equal(options);
     });
-
-    it.skip('should create a SenderBase with a valid request object', function() {
-      var senderBase = new SenderBase();
-      expect(senderBase.req).to.not.be.undefined;
-      expect(senderBase.req).to.be.a('Function');
-    });
   });
 
-  describe('sendBaseNoRetry', function() {
+  describe('sendBaseNoRetry', () => {
 
     // Set arguments passed in request proxy
     function setArgs(err, res, resBody) {
@@ -55,9 +42,9 @@ describe('UNIT SenderBase', function () {
       };
     };
 
-    var testRecipient = {registrationIds: ['myRegId']};
+    const testRecipient = { tokens: ['myRegId'] };
 
-    it.skip('should throw an error if no valid recipient is provided', function() {
+    it.skip('should throw an error if no valid recipient is provided', () => {
       var senderBase = new SenderBase(),
           callback = sinon.spy(),
           errorMsg = 'No recipients provided';
@@ -70,32 +57,32 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0]).to.equal(errorMsg);
     });
 
-    it.skip('should set proxy, maxSockets, and/or timeout of req object if passed into constructor', function() {
+    it.skip('should set proxy, maxSockets, and/or timeout of req object if passed into constructor', () => {
       var options = {
         proxy: 'http://myproxy.com',
         maxSockets: 100,
         timeout: 1000
       };
       var senderBase = new SenderBase('mykey', options);
-      senderBase.sendBaseNoRetry({ data: {} }, testRecipient, function () {});
+      senderBase.sendBaseNoRetry({ data: {} }, testRecipient, () => {});
       expect(args.options.proxy).to.equal(options.proxy);
       expect(args.options.maxSockets).to.equal(options.maxSockets);
       expect(args.options.timeout).to.equal(options.timeout);
     });
 
-    it.skip('should set the API key of req object if passed in API key', function () {
+    it.skip('should set the API key of req object if passed in API key', () => {
       var senderBase = new SenderBase('myKey');
-      senderBase.sendBaseNoRetry({ data: {} }, testRecipient, function () {});
+      senderBase.sendBaseNoRetry({ data: {} }, testRecipient, () => {});
       expect(args.options.headers.Authorization).to.equal('key=myKey');
     });
 
-    it.skip('should stringify body of req before it is sent', function () {
+    it.skip('should stringify body of req before it is sent', () => {
       var senderBase = new SenderBase('mykey');
-      senderBase.sendBaseNoRetry({ collapseKey: 'Message', data: {} }, testRecipient, function() {});
+      senderBase.sendBaseNoRetry({ collapseKey: 'Message', data: {} }, testRecipient, () => {});
       expect(args.options.body).to.be.a('string');
     });
 
-    it.skip('should set properties of body with message properties', function () {
+    it.skip('should set properties of body with message properties', () => {
       var mess = {
         collapseKey: 'Message',
         timeToLive: 100,
@@ -105,7 +92,7 @@ describe('UNIT SenderBase', function () {
         }
       };
       var senderBase = new SenderBase('mykey');
-      senderBase.sendBaseNoRetry(mess, testRecipient, function () {});
+      senderBase.sendBaseNoRetry(mess, testRecipient, () => {});
       var body = JSON.parse(args.options.body);
       expect(body[Constants.PARAM_COLLAPSE_KEY]).to.equal(mess.collapseKey);
       expect(body[Constants.PARAM_TIME_TO_LIVE]).to.equal(mess.timeToLive);
@@ -113,17 +100,17 @@ describe('UNIT SenderBase', function () {
       expect(body[Constants.PARAM_DATA_PAYLOAD_KEY]).to.deep.equal(mess.data);
     });
 
-    it.skip('should set registration ID body parameter if the recipient contains the registrationIds attribute', function() {
+    it.skip('should set registration ID body parameter if the recipient contains the registrationIds attribute', () => {
       var senderBase = new SenderBase('mykey');
-      senderBase.sendBaseNoRetry({data: {}}, testRecipient, function() {});
+      senderBase.sendBaseNoRetry({data: {}}, testRecipient, () => {});
       var body = JSON.parse(args.options.body);
       expect(body[Constants.JSON_REGISTRATION_IDS]).to.not.be.undefined;
       expect(body[Constants.JSON_REGISTRATION_IDS]).to.deep.equal(['myRegId']);
     });
 
-    it.skip('should set the "to" body parameter if the recipient contains the notificationKey attribute', function() {
+    it.skip('should set the "to" body parameter if the recipient contains the notificationKey attribute', () => {
       var senderBase = new SenderBase('mykey');
-      senderBase.sendBaseNoRetry({data: {}}, {notificationKey: 'notKey'}, function() {});
+      senderBase.sendBaseNoRetry({data: {}}, {notificationKey: 'notKey'}, () => {});
       var body = JSON.parse(args.options.body);
       expect(body[Constants.PARAM_TO]).to.not.be.undefined;
       expect(body[Constants.PARAM_TO]).to.equal('notKey');
@@ -131,7 +118,7 @@ describe('UNIT SenderBase', function () {
 
     it.skip('should set the registration ID body parameter if the recipient contains both registration IDs and a notification key');
 
-    it.skip('should pass an error into callback if request returns an error', function () {
+    it.skip('should pass an error into callback if request returns an error', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs('an error', {}, {});
@@ -140,7 +127,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.calledWith('an error')).to.be.ok;
     });
 
-    it.skip('should pass an error into callback if response does not exist', function () {
+    it.skip('should pass an error into callback if response does not exist', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs(null, undefined, {});
@@ -149,7 +136,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0]).to.equal('response is null');
     });
 
-    it.skip('should return an error with code 500 if returned a 500', function () {
+    it.skip('should return an error with code 500 if returned a 500', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs(null, { statusCode: 500 }, {});
@@ -159,7 +146,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0].code).to.equal(500);
     });
 
-    it.skip('should return an error with code 401 if returned a 401', function () {
+    it.skip('should return an error with code 401 if returned a 401', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs(null, { statusCode: 401 }, {});
@@ -169,7 +156,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0].code).to.equal(401);
     });
 
-    it.skip('should return an error with code 400 if returned a 400', function () {
+    it.skip('should return an error with code 400 if returned a 400', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs(null, { statusCode: 400 }, {});
@@ -179,7 +166,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0].code).to.equal(400);
     });
 
-    it.skip('should pass an error into the callback if resBody cannot be parsed', function () {
+    it.skip('should pass an error into the callback if resBody cannot be parsed', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
       setArgs(null, { statusCode: 200 }, "non-JSON string.");
@@ -188,7 +175,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0] instanceof Error).to.equal(true);
     });
 
-    it.skip('should pass in parsed resBody into callback on success', function () {
+    it.skip('should pass in parsed resBody into callback on success', () => {
       var callback = sinon.spy();
       var resBody = {
         message: 'woohoo!',
@@ -202,7 +189,7 @@ describe('UNIT SenderBase', function () {
     });
   });
 
-  describe('sendBase', function() {
+  describe('sendBase', () => {
     var restore = {},
         backoff = Constants.BACKOFF_INITIAL_DELAY;
 
@@ -215,7 +202,7 @@ describe('UNIT SenderBase', function () {
       };
     };
 
-    before(function () {
+    before(() => {
       restore.sendBaseNoRetry = SenderBase.prototype.sendBaseNoRetry;
       SenderBase.prototype.sendBaseNoRetry = function (message, recipient, callback) {
         console.log('Firing send');
@@ -229,11 +216,11 @@ describe('UNIT SenderBase', function () {
       };
     });
 
-    after( function () {
+    after( () => {
       SenderBase.prototype.sendBaseNoRetry = restore.sendBaseNoRetry;
     });
 
-    it.skip('should pass an error into callback if no valid recipient is specified', function() {
+    it.skip('should pass an error into callback if no valid recipient is specified', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey');
 
@@ -254,7 +241,7 @@ describe('UNIT SenderBase', function () {
       expect(callback.args[0][0]).to.equal('No recipient data provided!');
     });
 
-    it.skip('should pass the message and recipient to sendBaseNoRetry on call', function() {
+    it.skip('should pass the message and recipient to sendBaseNoRetry on call', () => {
       var callback = sinon.spy(),
           senderBase = new SenderBase('myKey'),
           message = "Test",
@@ -266,7 +253,7 @@ describe('UNIT SenderBase', function () {
       expect(args.tries).to.equal(1);
     });
 
-    it.skip('should pass the result into callback if successful for recipient', function () {
+    it.skip('should pass the result into callback if successful for recipient', () => {
       var callback = sinon.spy(),
           result = { success: true },
           senderBase = new SenderBase('myKey');
@@ -279,7 +266,7 @@ describe('UNIT SenderBase', function () {
 
     it.skip('should retry number of times passed into call and do exponential backoff', function (done) {
       var start = new Date();
-      var callback = function () {
+      var callback = () => {
         expect(args.tries).to.equal(2);
         expect(new Date() - start).to.be.gte(Math.pow(2, 0) * backoff);
         done();
@@ -291,7 +278,7 @@ describe('UNIT SenderBase', function () {
 
     it.skip('should retry all recipient regIds in event of an error', function (done) {
       var start = new Date();
-      var callback = function () {
+      var callback = () => {
         expect(args.tries).to.equal(2);
         expect(args.reg_ids.length).to.equal(3);
         done();
@@ -303,7 +290,7 @@ describe('UNIT SenderBase', function () {
 
     it.skip('should retry with the same notification key in event of an error', function (done) {
       var start = new Date();
-      var callback = function () {
+      var callback = () => {
         expect(args.tries).to.equal(2);
         expect(args.recipient.notificationKey).to.equal('test');
         done();
@@ -374,7 +361,7 @@ describe('UNIT SenderBase', function () {
         });
     });
 
-    it.skip('should pass the error into callback if failure and no retry for recipient', function () {
+    it.skip('should pass the error into callback if failure and no retry for recipient', () => {
       var callback = sinon.spy(),
           error = 'my error',
           senderBase = new SenderBase('myKey');
